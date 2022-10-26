7ad28bf53a63593b743928dcf429f11882f84352
Kiann <pheekianann@hotmail.com>
2022-10-26 21:41:37 +0800

    DataStoreManager and serialization

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-import Controllers.*;
+import Controllers.MainController;
+import Controllers.NavigationController;
+import Models.DataStoreManager;
 
 class Main {
 
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		System.out.println("Application Starting...");
 		// Load DB
-
+		DataStoreManager.getInstance().LoadAll();
 		// Load Initial Controller
 		NavigationController.getInstance().Load(new MainController());
 
