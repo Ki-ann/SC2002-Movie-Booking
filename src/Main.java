b8682f5a5146d70d12fd6bba587a16ce92cb2eb6
Kiann <pheekianann@hotmail.com>
2022-11-02 01:21:52 +0800

    Revert Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ class Main {
 	public static void main(String[] args) {
 		System.out.println("Application Starting...");
 		// Load DB
-		//DataStoreManager.getInstance().loadAll();
+		DataStoreManager.getInstance().loadAll();
 		// Load Initial Controller
 		NavigationController.getInstance().load(new MainController());
 
