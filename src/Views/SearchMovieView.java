0fa346b968587dc76de7484086162617c9b317c3
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2022-11-05 11:21:24 +0800

    fixed showing of movies

--- src/Views/SearchMovieView.java
+++ src/Views/SearchMovieView.java
@@ -1,4 +1,9 @@
 package Views;
 
 public class SearchMovieView {
+    public static String searchOptions() {
+        ConsoleIOManager.printLine("Enter movie name: ");
+        String name = ConsoleIOManager.readString();
+        return name;
+    }
 }
